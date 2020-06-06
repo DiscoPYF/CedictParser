1b91197beecb98e8b487f053b2242a0e5043a0e0
Pierre-Yves FARE <pyfare@gmail.com>
2020-06-06 19:36:50 +0100

    Add csproj and a parser interface.

---
+++ CedictParser/CedictEntry.cs
@@ -0,0 +1,28 @@
+﻿namespace CedictParser
+{
+    /// <summary>
+    /// Represents one CEDICT entry.
+    /// </summary>
+    public class CedictEntry
+    {
+        /// <summary>
+        /// The word in traditional Chinese.
+        /// </summary>
+        public string Traditional { get; set; }
+
+        /// <summary>
+        /// The word in simplified Chinese.
+        /// </summary>
+        public string Simplified { get; set; }
+
+        /// <summary>
+        /// The Mandarin pinyin.
+        /// </summary>
+        public string Pinyin { get; set; }
+
+        /// <summary>
+        /// The English definitions in American English.
+        /// </summary>
+        public string[] Definitions { get; set; }
+    }
+}
---
+++ CedictParser/ICedictParser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CedictParser
+{
+    /// <summary>
+    /// Defines a CEDICT parser.
+    /// </summary>
+    public interface ICedictParser
+    {
+        /// <summary>
+        /// Reads the next CEDICT entry.
+        /// </summary>
+        /// <returns>The next CEDICT entry or null
+        /// if there is nothing left to read.</returns>
+        CedictEntry Read();
+
+        /// <summary>
+        /// Read all the CEDICT entries.
+        /// </summary>
+        /// <returns>A list of CEDICT entries or null
+        /// if there is nothing left to read.</returns>
+        IList<CedictEntry> ReadAll();
+    }
+}
