ce2e6573d75cd3d8d193c310b97ea38a2c52d55e
Pierre-Yves FARE <pyfare@gmail.com>
2020-06-11 21:57:45 +0100

    Add tests for empty content.

--- CedictParser.Tests/CedictParserTests.cs
+++ CedictParser.Tests/CedictParserTests.cs
@@ -71,6 +71,25 @@ namespace CedictParserLib.Tests
         }
 
         [TestMethod]
+        public void Read_ShouldReturnNull_WhenNothingToRead()
+        {
+            string s = "";
+
+            byte[] b = Encoding.UTF8.GetBytes(s);
+
+            var stream = new MemoryStream(b);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var parser = new CedictParser(reader);
+
+                CedictEntry entry = parser.Read();
+
+                Assert.IsNull(entry);
+            }
+        }
+
+        [TestMethod]
         public void Read_ShouldReturnEntryWithDefaultPropertyValues_WhenEntryIsMalformed()
         {
             string s = "你好你好 [ni3 hao3] /hello/hi/";
@@ -182,6 +201,25 @@ namespace CedictParserLib.Tests
         }
 
         [TestMethod]
+        public async Task ReadAsync_ShouldReturnNull_WhenNothingToRead()
+        {
+            string s = "";
+
+            byte[] b = Encoding.UTF8.GetBytes(s);
+
+            var stream = new MemoryStream(b);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var parser = new CedictParser(reader);
+
+                CedictEntry entry = await parser.ReadAsync();
+
+                Assert.IsNull(entry);
+            }
+        }
+
+        [TestMethod]
         public async Task ReadAsync_ShouldReturnEntryWithDefaultPropertyValues_WhenEntryIsMalformed()
         {
             string s = "你好你好 [ni3 hao3] /hello/hi/";
@@ -275,6 +313,26 @@ namespace CedictParserLib.Tests
         }
 
         [TestMethod]
+        public void ReadToEnd_ShouldReturnEmpty_WhenNothingToRead()
+        {
+            string s = "";
+
+            byte[] b = Encoding.UTF8.GetBytes(s);
+
+            var stream = new MemoryStream(b);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var parser = new CedictParser(reader);
+
+                IList<CedictEntry> entries = parser.ReadToEnd();
+
+                Assert.IsNotNull(entries);
+                Assert.AreEqual(0, entries.Count);
+            }
+        }
+
+        [TestMethod]
         public async Task ReadToEndAsync_ShouldReturnAllEntries()
         {
             string s = "你好 你好 [ni3 hao3] /hello/hi/\n" +
@@ -314,6 +372,26 @@ namespace CedictParserLib.Tests
                 Assert.AreEqual(2, entry.Definitions.Length);
                 Assert.AreEqual("goodbye", entry.Definitions[0]);
                 Assert.AreEqual("see you again later", entry.Definitions[1]);
+            }
+        }
+
+        [TestMethod]
+        public async Task ReadToEndAsync_ShouldReturnEmpty_WhenNothingToRead()
+        {
+            string s = "";
+
+            byte[] b = Encoding.UTF8.GetBytes(s);
+
+            var stream = new MemoryStream(b);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var parser = new CedictParser(reader);
+
+                IList<CedictEntry> entries = await parser.ReadToEndAsync();
+
+                Assert.IsNotNull(entries);
+                Assert.AreEqual(0, entries.Count);
             }
         }
     }
